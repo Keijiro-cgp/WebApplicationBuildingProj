c04ac1f4e84a7ad6c540d9e3d6e9f0f01b2267b2
Ririri15_niko <77618140+ririri15@users.noreply.github.com>
2021-07-19 17:01:14 +0900

    Update tab.js
    tab.jsの修正

--- src/main/webapp/tab.js
+++ src/main/webapp/tab.js
@@ -1,10 +1,26 @@
-$(function(){
-  $("#js-tab li").click(function(){
-    $("#js-tab li").removeClass("active");
-    $(this).addClass("active");
-    var index = $(this).index();
+document.addEventListener('DOMContentLoaded', function(){
+  tabs = document.querySelectorAll('#js-tab li');
+  for(i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', tabSwitch, false);
+  }
 
-    $(".tab-content").removeClass("active");
-    $(".tab-content").eq(index).addClass("active");
-  });
+  function tabSwitch(){
+    tabs = document.querySelectorAll('#js-tab li');
+    var node = Array.prototype.slice.call(tabs, 0);
+    node.forEach(function (element) {
+      element.classList.remove('active');
+    });
+    this.classList.add('active');
+
+    content = document.querySelectorAll('.tab-content');
+    var node = Array.prototype.slice.call(content, 0);
+    node.forEach(function (element) {
+      element.classList.remove('active');
+    });
+
+    const arrayTabs = Array.prototype.slice.call(tabs);
+    const index = arrayTabs.indexOf(this);
+    
+    document.querySelectorAll('.tab-content')[index].classList.add('active');
+  };
 });
