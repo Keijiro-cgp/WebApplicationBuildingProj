292ac8f23cfa1e122b936a944d75899143cbab5b
CLAY-GALLERIA\dscwx <dscwx777@yahoo.co.jp>
2021-07-26 00:25:40 +0900

    大規模変更している途中だけど念のためプッシュ

--- src/main/webapp/DandD.js
+++ src/main/webapp/DandD.js
@@ -1,80 +1,97 @@
 (function(){
 
-    //要素の取得
-    var elements = document.getElementsByClassName("drag-and-drop");
+//要素の取得
+	var elements = document.getElementsByClassName("drag-and-drop");
 
-    //要素内のクリックされた位置を取得するグローバル（のような）変数
-    var x;
-    var y;
+//要素内のクリックされた位置を取得するグローバル（のような）変数
+	var x;
+	var y;
 
-    //マウスが要素内で押されたとき、又はタッチされたとき発火
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
-    }
+//マウスが要素内で押されたとき、又はタッチされたとき発火
+	for(var i = 0; i < elements.length; i++) {
+		elements[i].addEventListener("mousedown", mdown, false);
+		elements[i].addEventListener("touchstart", mdown, false);
+	}
 
-    //マウスが押された際の関数
-    function mdown(e) {
+//マウスが押された際の関数
+	function mdown(e) {
+//クラス名に .drag を追加
+		this.classList.add("drag");
+//タッチデイベントとマウスのイベントの差異を吸収
+		if(e.type === "mousedown") {
+			var event = e;
+		} else {
+			var event = e.changedTouches[0];
+		}
+//要素内の相対座標を取得
+		x = event.pageX - this.offsetLeft;
+		y = event.pageY - this.offsetTop;
+//ムーブイベントにコールバック
+		document.body.addEventListener("mousemove", mmove, false);
+		document.body.addEventListener("touchmove", mmove, false);
+	}
 
-        //クラス名に .drag を追加
-        this.classList.add("drag");
+//マウスカーソルが動いたときに発火
+	function mmove(e) {
+//ドラッグしている要素を取得
+		var drag = document.getElementsByClassName("drag")[0];
+//同様にマウスとタッチの差異を吸収
+		if(e.type === "mousemove") {
+			var event = e;
+		} else {
+			var event = e.changedTouches[0];
+		}
+//フリックしたときに画面を動かさないようにデフォルト動作を抑制
+		e.preventDefault();
+//マウスが動いた場所に要素を動かす
+		drag.style.top = event.pageY - y + "px";
+		drag.style.left = event.pageX - x + "px";
+//マウスボタンが離されたとき、またはカーソルが外れたとき発火
+		drag.addEventListener("mouseup", mup, false);
+		document.body.addEventListener("mouseleave", mup, false);
+		drag.addEventListener("touchend", mup, false);
+		document.body.addEventListener("touchleave", mup, false);
 
-        //タッチデイベントとマウスのイベントの差異を吸収
-        if(e.type === "mousedown") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
+	}
 
-        //要素内の相対座標を取得
-        x = event.pageX - this.offsetLeft;
-        y = event.pageY - this.offsetTop;
+//マウスボタンが上がったら発火
+	function mup(e) {
+		var drag = document.getElementsByClassName("drag")[0];
+//ムーブベントハンドラの消去
+		document.body.removeEventListener("mousemove", mmove, false);
+		drag.removeEventListener("mouseup", mup, false);
+		document.body.removeEventListener("touchmove", mmove, false);
+		drag.removeEventListener("touchend", mup, false);
+//クラス名 .drag も消す
+		drag.classList.remove("drag");
+	}
 
-        //ムーブイベントにコールバック
-        document.body.addEventListener("mousemove", mmove, false);
-        document.body.addEventListener("touchmove", mmove, false);
-    }
+//タブ4操作
 
-    //マウスカーソルが動いたときに発火
-    function mmove(e) {
+	function onDragStart(event) {
+		event
+			.dataTransfer
+			.setData('text/plain', event.target.id);
+		event
+			.currentTarget
+			.style
+			.backgroundColor = 'yellow';
+	}
 
-        //ドラッグしている要素を取得
-        var drag = document.getElementsByClassName("drag")[0];
+	function onDragOver(event) {
+		event.preventDefault();
+	}
 
-        //同様にマウスとタッチの差異を吸収
-        if(e.type === "mousemove") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
-        e.preventDefault();
-
-        //マウスが動いた場所に要素を動かす
-        drag.style.top = event.pageY - y + "px";
-        drag.style.left = event.pageX - x + "px";
-
-        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
-        drag.addEventListener("mouseup", mup, false);
-        document.body.addEventListener("mouseleave", mup, false);
-        drag.addEventListener("touchend", mup, false);
-        document.body.addEventListener("touchleave", mup, false);
-
-    }
-
-    //マウスボタンが上がったら発火
-    function mup(e) {
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //ムーブベントハンドラの消去
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
-
-        //クラス名 .drag も消す
-        drag.classList.remove("drag");
-    }
+	function onDrop(event) {
+		const id = event
+			.dataTransfer
+			.getData('text');
+		const draggableElement = document.getElementById(id);
+		const dropzone = event.target;
+		dropzone.appendChild(draggableElement);
+		event
+			.dataTransfer
+			.clearData();
+	}
 
 })()
