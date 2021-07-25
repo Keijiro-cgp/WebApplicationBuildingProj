be700f4719d03034f5e4183c6409c75ce2a453ec
CLAY-GALLERIA\dscwx <dscwx777@yahoo.co.jp>
2021-07-26 01:24:18 +0900

    ファイルの追加、htmlの変更

--- src/main/webapp/DandD.js
+++ src/main/webapp/DandD.js
@@ -66,31 +66,3 @@
 		drag.classList.remove("drag");
 	}
 })()
-
-//タブ4操作
-
-	function onDragStart(event) {
-		event
-			.dataTransfer
-			.setData('text/plain', event.target.id);
-		event
-			.currentTarget
-			.style
-			.backgroundColor = 'yellow';
-	}
-
-	function onDragOver(event) {
-		event.preventDefault();
-	}
-
-	function onDrop(event) {
-		const id = event
-			.dataTransfer
-			.getData('text');
-		const draggableElement = document.getElementById(id);
-		const dropzone = event.target;
-		dropzone.appendChild(draggableElement);
-		event
-			.dataTransfer
-			.clearData();
-	}
